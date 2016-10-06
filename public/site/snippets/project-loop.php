540cf9922edbd07d1e21419125da036262822852
Sebastian Graz <sebastian@Sebastians-MacBook-Pro-2.local>
2016-10-06 14:03:53 +0100

    added structured content blueprint

--- public/site/snippets/project-loop.php
+++ public/site/snippets/project-loop.php
@@ -1,5 +1,5 @@
 <ul class="projects">
-  <?php foreach(page('projects')->children()->visible()->limit(3) as $project): ?>
+  <?php foreach(page('projects')->children()->visible()->limit($count) as $project): ?>
   <li>
     <h3><a href="<?php echo $project->url() ?>"><?php echo $project->title()->html() ?></a></h3>
     <p><?php echo $project->text()->excerpt(80) ?> <a href="<?php echo $project->url() ?>">read&nbsp;more&nbsp;→</a></p>
